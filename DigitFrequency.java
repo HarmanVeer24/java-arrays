b25822ac16a2edbe7d34141252f5ca97d82e451a
HarmanVeer24 <harmanveer0024@gmail.com>
2025-03-03 23:12:00 +0530

    [Harman] ADD:program to print frequencies of each digit of given input number

---
+++ DigitFrequency.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DigitFrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        // Array to store frequency of each digit (0-9)
+        int[] frequency = new int[10];
+
+        // Count frequency of each digit
+        while (num > 0) {
+            int digit = num % 10;
+            frequency[digit]++;
+            num /= 10;
+        }
+
+        // Display digit frequencies
+        System.out.println("Digit | Frequency");
+        for (int i = 0; i < 10; i++) {
+            if (frequency[i] > 0) {
+                System.out.println(i + " | " + frequency[i]);
+            }
+        }
+    }
+}
